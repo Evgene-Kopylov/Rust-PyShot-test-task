23316a5561f8630b2a37dc47c7546d4f15eea176
Evgene-Kopylov <eugenevkopylov@gmail.com>
2023-09-08 19:38:02 +0200

    wip: распределение чисел по потокам

--- hash_finder/src/main.rs
+++ hash_finder/src/main.rs
@@ -22,12 +22,14 @@ struct Args {
     lines: usize,
 }
 
+/// Возвращает строку хеша для числа.
 fn calculate_sha256_hash(value: u64) -> String {
     let mut hasher = Sha256::new();
     hasher.update(value.to_be_bytes());
     encode(hasher.finalize())
 }
 
+/// Проверяет точное число нулей на конце строки.
 fn ends_with_zeros(hash_string: &str, n: usize) -> bool {
     hash_string.ends_with(&"0".repeat(n))
 }
@@ -43,12 +45,18 @@ fn main() {
     // Находим количество доступных ядер CPU
     let num_threads = num_cpus::get();
     
-    for thread in 0..num_threads {
+    for i in 0..num_threads {
         let results = Arc::clone(&results);
         let handle = thread::spawn(move || {
-            let line = "737363636700000";
-            let mut val = results.lock().unwrap();
-            val.push(line);
+            let mut val = results.lock().unwrap();            
+            for j in (0..10_000_000).step_by(num_threads) {
+                let hash_string = calculate_sha256_hash((j + i) as u64);
+                if ends_with_zeros(&hash_string, args.zeros) {
+                    let line = format!("{} {}", j.to_string().yellow(), hash_string.green());
+                    val.push(line);
+                    break;
+                }
+            }            
         });
         handles.push(handle);
 
@@ -62,71 +70,6 @@ fn main() {
     for line in res.iter() {
         println!("{}", line);
     }
-
-    // // Находим количество доступных ядер CPU
-    // let cpu_threads = num_cpus::get();
-    // println!("cpu_threads = {}", cpu_threads);
-
-    // for value in 0..1_000_000 {
-    //     let hash_string = calculate_sha256_hash(value);
-    //     if ends_with_zeros(&hash_string, args.zeros) {
-    //         println!("{}, \"{}\"", value.to_string().yellow(), hash_string.green());
-    //         break;
-    //     }
-
-    // }
-
-
-    // let num_values_per_thread = args.zeros / num_threads;
-
-    // // Создаем вектор для хранения результатов
-    // let results = Arc::new(Mutex::new(Vec::new()));
-
-    // // Создаем вектор потоков
-    // let mut handles = vec![];
-
-    // for _ in 0..num_threads {
-    //     let results = Arc::clone(&results);
-
-    //     let handle = std::thread::spawn(move || {
-    //         let mut count = 0;
-    //         let mut value: u64 = 1; // Используем u64 для избегания переполнения
-
-    //         while count < num_values_per_thread {
-    //             // Рассчитываем хеш SHA-256 для числа
-    //             let hash_hex = calculate_sha256_hash(value);
-
-    //             // Проверяем, оканчивается ли хеш на нули
-    //             if ends_with_zeros(&hash_hex, args.zeros) {
-    //                 let result_string = format!("{}, \"{}\"", value, hash_hex);
-    //                 let mut results = results.lock().unwrap();
-    //                 results.push(result_string);
-    //                 dbg!(results);
-    //             }
-
-    //             value += 1;
-    //             count += 1;
-    //         }
-    //     });
-
-    //     handles.push(handle);
-    // }
-
-    // // Дожидаемся завершения всех потоков
-    // for handle in handles {
-    //     handle.join().unwrap();
-    //     println!("op");
-    // }
-
-    // // Выводим результаты
-    // let results = results.lock().unwrap();
-    // println!("fin");
-    // dbg!(results);
-    // // for result in results.iter() {
-    // //     println!("i");
-    // //     println!("{}", result);
-    // // }
-
 }
 
 #[cfg(test)]
@@ -143,7 +86,7 @@ mod tests {
     #[test]
     fn test_ends_with_zeros() {
         let hash_string = "12345abcde000000";
-        assert!(ends_with_zeros(hash_string, 5)); // Хеш должен оканчиваться на 5 нулей
-        assert!(!ends_with_zeros(hash_string, 7)); // Хеш не должен оканчиваться на 6 нулей
+        assert!(ends_with_zeros(hash_string, 6)); // Хеш должен оканчиваться на 6 нулей
+        assert!(!ends_with_zeros(hash_string, 7)); // Хеш не должен оканчиваться на 7 нулей
     }
 }
